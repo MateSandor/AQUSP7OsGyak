67633b490e59f73fc7aab113e262ab36ee5c53ea
MateSandor <79148317+MateSandor@users.noreply.github.com>
2021-03-09 17:15:53 +0100

    5. gyakorlat feltöltése

---
+++ AQUSP7_03_09/segedlet5gyakhoz/child.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main (void) {
+	int i=0;
+        for(i=0;i<5;i++){
+        printf("Sandor Mate AQUSP7\n");
+        }
+        return 0;
+}
